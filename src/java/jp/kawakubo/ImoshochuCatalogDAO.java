1485ee974bd22abdabf5d3f112ee4cab2afbf7b1
HAJIME Fukuna <f97one@gmail.com>
2013-03-26 23:15:22 +0900

    DAOインターフェースを作成。

---
+++ src/java/jp/kawakubo/ImoshochuCatalogDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.kawakubo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kazutoshi
+ */
+public interface ImoshochuCatalogDAO {
+    
+    public ArrayList getImoshochuCatalogList();
+}
